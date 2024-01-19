de4552e665c591c1d917cdd4d0cb579cfa9348e3
shirleyisaza <127167701+shirleyisaza@users.noreply.github.com>
2024-01-19 00:25:43 -0500

    configuración
    ok

--- principal/settings.py
+++ principal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'principal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["principal/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -116,8 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "static"),   
+]
+STATIC_ROOT = "/static"
+MEDIA_URL= '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")   
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
